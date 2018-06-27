fff283c7c8fcada5af52b5f04b2757aae8f7dde4
Silence <chenmohaha_2000@163.com>
2018-06-27 09:10:43 +0800

    修改列表页BUG

--- spring-mybatis/src/main/java/com/silence/code/generator/sm/model/Common.java
+++ spring-mybatis/src/main/java/com/silence/code/generator/sm/model/Common.java
@@ -8,6 +8,12 @@ public class Common {
 
     private String listForEach = "#foreach(${item} in $!{pageData.result})";
     private String listEnd = "#end";
+    private String meta = "#parse(\"common/meta.html\")";
+    private String header = "#parse(\"common/header.html\")";
+    private String leftMenu = "#parse(\"common/left_menu.html\")";
+    private String ifTotalCount = "#if($!pageData.totalCount > 0)";
+    private String showPage = "#showPage($!{pageData},\"/admin/healthCircle/list?\")";
+    private String footer = "#parse(\"common/footer.html\")";
 
     public String getListForEach() {
         return listForEach;
@@ -25,4 +31,51 @@ public class Common {
         this.listEnd = listEnd;
     }
 
+    public String getMeta() {
+        return meta;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getLeftMenu() {
+        return leftMenu;
+    }
+
+    public void setLeftMenu(String leftMenu) {
+        this.leftMenu = leftMenu;
+    }
+
+    public String getIfTotalCount() {
+        return ifTotalCount;
+    }
+
+    public void setIfTotalCount(String ifTotalCount) {
+        this.ifTotalCount = ifTotalCount;
+    }
+
+    public String getShowPage() {
+        return showPage;
+    }
+
+    public void setShowPage(String showPage) {
+        this.showPage = showPage;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
 }
